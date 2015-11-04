857e2e282c7f49e76ca9fd657310dfcc84297451
Chet Mancini <chetmancini@gmail.com>
2015-11-04 14:28:10 -0500

    CM: first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# data-transit
-Basic demonstrations of moving values from different languages.
+# Data Transit
+
+## Usage
+
+
+## License
+
+Copyright © 2015 Chet Mancini
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject data-transit "0.1.0-SNAPSHOT"
+  :description "Demonstration of moving data around"
+  :url "http://github.com/chetmancini/data-transit"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.intentmedia/schema-transform "0.0.1-SNAPSHOT"])
