3aa481b510bc87efa4e88b9f62d041d2dc561be8
Chet Mancini <chetmancini@gmail.com>
2015-11-04 21:52:16 -0500

    CM: update project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.intentmedia/schema-transform "0.0.1-SNAPSHOT"])
+                 [prismatic/schema "1.0.3"]
+                 [com.intentmedia/schema-transform "0.0.1-SNAPSHOT"]])
